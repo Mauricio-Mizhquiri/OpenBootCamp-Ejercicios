a11cc02354e51ae7c27222d0f4a35ed968b4a3df
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-09-01 23:35:12 -0500

    terminando el ejercicio del modulo de base de datos de python

--- Python/Base de datos/Ejercicios/ejercicio.py
+++ Python/Base de datos/Ejercicios/ejercicio.py
@@ -1,6 +1,27 @@
 import sqlite3
+from typing import Tuple
+
 
 def main():
+    name = input('Ingrese nombre: ')
+    apellido = input("Ingrese apellido: ")
+    realizarBusqueda(name, apellido)
+
+def realizarBusqueda(nombre, apellido):
+    conn = sqlite3.connect('C:/Users/rolan/Escritorio/OpenBootCamp/Python/Base de datos/Ejercicios/Alumnos.db')
+    cursor = conn.cursor()
+    query = f'SELECT * FROM Alumnos WHERE nombre = "{nombre}" AND apellido = "{apellido}"'
+    rows = cursor.execute(query)
+    data = rows.fetchone()
+    if isinstance(data, Tuple):
+        print('\t\nAlumno encontado sus datos son:\n')
+        print('Id: ',data[0])
+        print('Nombre: ',data[1])
+        print('Apellido: ',data[2])
+    else:
+        print('Alumno no encontrado')	
+
+def main2():
     for i in range(4):
         id = int(input('Ingrese el id del alumno: '))
         name = input("Ingrese el nombre del alumno: ")
