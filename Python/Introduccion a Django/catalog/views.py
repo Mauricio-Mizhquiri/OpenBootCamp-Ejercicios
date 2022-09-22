c5056243976feb6d3a1129b2ed7e700e0d558ebf
Mauricio-Mizhquiri <rolando.mizhquiri@gmail.com>
2022-09-22 00:34:15 -0500

    ultimos archivos de Django

--- Python/Introduccion a Django/catalog/views.py
+++ Python/Introduccion a Django/catalog/views.py
@@ -1,3 +1,19 @@
+from telnetlib import STATUS
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_boks = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_authors = Author.objects.all().count()
+
+    disponibles = BookInstance.objects.filter(status__exact = 'a').count()
+    return render(request, "index.html", context={"num_books":num_boks,
+                            "num_authors":num_authors,
+                            "disponibles":disponibles,
+                            "num_instances":num_instances
+    }
+    )
